a7e51980a446c91793de3202d8e9461d5259a661
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-01-11 13:21:00 +0530

    changed infra/data/security.tf used local instead of var

--- infra/data/security.tf
+++ infra/data/security.tf
@@ -1,8 +1,6 @@
-# Shared app access security group
-# Apps (EKS later, or anything else) attach to this
 resource "aws_security_group" "app_access" {
   name   = "${local.name_prefix}-app-access-sg"
-  vpc_id = var.vpc_id
+  vpc_id = local.vpc_id
 
   egress {
     from_port   = 0
@@ -12,10 +10,9 @@ resource "aws_security_group" "app_access" {
   }
 }
 
-# RDS security group
 resource "aws_security_group" "rds" {
   name   = "${local.name_prefix}-rds-sg"
-  vpc_id = var.vpc_id
+  vpc_id = local.vpc_id
 
   ingress {
     from_port       = 5432
@@ -32,10 +29,9 @@ resource "aws_security_group" "rds" {
   }
 }
 
-# Redis security group
 resource "aws_security_group" "redis" {
   name   = "${local.name_prefix}-redis-sg"
-  vpc_id = var.vpc_id
+  vpc_id = local.vpc_id
 
   ingress {
     from_port       = 6379
