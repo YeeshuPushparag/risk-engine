9a75f2cb31b7cece83809920c6b1b4355b0e20d4
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-01-11 18:25:21 +0530

    added eks_node_security_group_id in data/outputs.tf

--- infra/data/outputs.tf
+++ infra/data/outputs.tf
@@ -9,3 +9,7 @@ output "redis_endpoint" {
 output "app_access_sg_id" {
   value = aws_security_group.app_access.id
 }
+
+output "eks_node_security_group_id" {
+  value = module.eks.node_security_group_id
+}
