e8e6e5637a07df73ea88bef2cfab630c29fe38d0
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-01-15 03:43:44 +0530

    feat: add route53 records for app and api subdomains

--- infra/platform/route53.tf
+++ infra/platform/route53.tf
@@ -6,4 +6,37 @@ resource "aws_route53_zone" "pushparag" {
   }
 }
 
+# ------------------------------------------------------------
+# Route53 Records -> ALB (EKS Ingress)
+# ------------------------------------------------------------
+locals {
+  alb_dns_name = "k8s-riskengine-ca22df56e5-778505082.us-east-1.elb.amazonaws.com"
+  alb_zone_id  = "Z35SXDOTRQ7X7K" # ALB Hosted Zone ID for us-east-1
+}
+
+resource "aws_route53_record" "app" {
+  zone_id = aws_route53_zone.pushparag.zone_id
+  name    = "app.pushparag.online"
+  type    = "A"
+
+  alias {
+    name                   = local.alb_dns_name
+    zone_id                = local.alb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = aws_route53_zone.pushparag.zone_id
+  name    = "api.pushparag.online"
+  type    = "A"
+
+  alias {
+    name                   = local.alb_dns_name
+    zone_id                = local.alb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+
 
