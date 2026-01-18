792bdabce7c7bea743765435ac648ce3b7a1478a
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-01-18 18:08:38 +0530

    change kafka stateful

---
+++ infra/eks/rbac-jenkins.tf
@@ -0,0 +1,48 @@
+resource "kubernetes_namespace" "jenkins" {
+  metadata {
+    name = "jenkins"
+  }
+}
+
+resource "kubernetes_service_account" "jenkins_agent" {
+  metadata {
+    name      = "jenkins-agent"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+}
+
+resource "kubernetes_cluster_role" "jenkins_agent_role" {
+  metadata {
+    name = "jenkins-agent-role"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "pods/exec", "pods/log", "secrets", "configmaps"]
+    verbs      = ["get", "list", "watch", "create", "delete", "patch", "update"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "deployments/scale"]
+    verbs      = ["get", "list", "watch", "patch", "update"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "jenkins_agent_rolebinding" {
+  metadata {
+    name = "jenkins-agent-rolebinding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.jenkins_agent_role.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.jenkins_agent.metadata[0].name
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+}
