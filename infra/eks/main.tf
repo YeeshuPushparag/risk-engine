b978925cb1f9d6d5f21ef4b598a29cb16cb7efd4
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-01-20 22:33:58 +0530

    Expose Jenkins public endpoint

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids              = var.subnet_ids
+    subnet_ids = var.subnet_ids
   }
 
   depends_on = [
