548a26d4cc055fc45fb9db0d73aa71f49c3877bb
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-01-26 15:30:53 +0530

    changed airflow configmap

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -35,6 +35,12 @@ CSRF_TRUSTED_ORIGINS = os.environ.get(
     "http://localhost:3000,http://localhost:8000"
 ).split(",")
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_COOKIE_SAMESITE = "Lax"       # Works for same-site login
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+
 # Application definition
 
 INSTALLED_APPS = [
