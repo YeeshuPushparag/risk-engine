dfabe01cad07cb76efaae3c614a5a539ed02cc00
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-02-02 22:47:08 +0530

    testing airflow

--- infra/platform/storage/s3_analytics.tf
+++ infra/platform/storage/s3_analytics.tf
@@ -20,34 +20,3 @@ resource "aws_s3_bucket_versioning" "risk_analytics_data" {
   }
 }
 
-
-# =========================
-# S3 Bucket for Airflow Logs
-# =========================
-resource "aws_s3_bucket" "airflow_logs" {
-  bucket = "pushparag-airflow-logs"
-
-  # Optional: enforce encryption
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Name        = "pushparag-airflow-logs"
-    Project     = "risk"
-    Environment = "platform"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "airflow_logs" {
-  bucket = aws_s3_bucket.airflow_logs.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
