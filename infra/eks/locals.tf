2b7e9379112212112acb29a42dcc13a13712c380
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-02-14 18:15:48 +0530

    changed subnet from private to public

--- infra/eks/locals.tf
+++ infra/eks/locals.tf
@@ -1,5 +1,5 @@
 locals {
   oidc_issuer = replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
-  eks_subnet_ids = data.terraform_remote_state.platform.outputs.private_subnet_ids
+  eks_subnet_ids = data.terraform_remote_state.platform.outputs.public_subnets
   vpc_id         = data.terraform_remote_state.platform.outputs.vpc_id
 }
