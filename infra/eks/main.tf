a99d5b2b0e4d149d7716c59a94db465318aa7a84
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-02-17 11:24:30 +0530

    testing airflow

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -79,8 +79,8 @@ resource "aws_eks_node_group" "web" {
   instance_types = ["t3.medium"]
 
   scaling_config {
-    min_size     = 1
-    desired_size = 1
+    min_size     = 0
+    desired_size = 0
     max_size     = 1
   }
 
