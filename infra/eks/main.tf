a5976c9743fe52e6a6df72cd8217d445834a4fec
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-02-27 16:57:24 +0530

    testing airflow

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -50,7 +50,7 @@ resource "aws_eks_node_group" "platform_core" {
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = local.eks_subnet_ids
 
-  instance_types = ["t3.large"]
+  instance_types = ["t3.xlarge"]
 
   scaling_config {
     min_size     = 1
