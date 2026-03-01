0259dfeb3f89e9c12a73b61dcdb228cfa3a27f9b
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-03-01 19:48:46 +0530

    monitoring

---
+++ infra/eks/irsa-prometheus.tf
@@ -0,0 +1,51 @@
+############################################
+# Prometheus IRSA Role (CloudWatch Access)
+############################################
+resource "aws_iam_role" "prometheus_irsa_role" {
+  name = "${var.cluster_name}-prometheus-irsa-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${local.oidc_issuer}:sub" = "system:serviceaccount:monitoring:prometheus"
+        }
+      }
+    }]
+  })
+}
+
+############################################
+# CloudWatch Read Policy
+############################################
+resource "aws_iam_policy" "prometheus_cloudwatch_policy" {
+  name = "${var.cluster_name}-prometheus-cloudwatch-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "cloudwatch:GetMetricData",
+        "cloudwatch:GetMetricStatistics",
+        "cloudwatch:ListMetrics",
+        "tag:GetResources"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+############################################
+# Attach Policy to Role
+############################################
+resource "aws_iam_role_policy_attachment" "prometheus_cloudwatch_attach" {
+  role       = aws_iam_role.prometheus_irsa_role.name
+  policy_arn = aws_iam_policy.prometheus_cloudwatch_policy.arn
+}
--- infra/eks/namespaces.tf
+++ infra/eks/namespaces.tf
@@ -9,3 +9,9 @@ resource "kubernetes_namespace_v1" "streaming" {
     name = "streaming"
   }
 }
+
+resource "kubernetes_namespace_v1" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
