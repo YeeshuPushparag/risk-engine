7fd9838cdb536e7bb7803a2f6362b295349bc17c
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-03-02 20:04:39 +0530

    testing monitoring

--- infra/eks/irsa-prometheus.tf
+++ infra/eks/irsa-prometheus.tf
@@ -8,25 +8,35 @@ resource "aws_iam_role" "prometheus_irsa_role" {
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
+
       Principal = {
         Federated = aws_iam_openid_connect_provider.eks.arn
       }
+
       Action = "sts:AssumeRoleWithWebIdentity"
 
       Condition = {
         StringEquals = {
+          # REQUIRED by STS
           "${local.oidc_issuer}:aud" = "sts.amazonaws.com"
         }
 
         StringLike = {
+          # Allow Prometheus SA
           "${local.oidc_issuer}:sub" = [
             "system:serviceaccount:monitoring:prometheus",
+
+            # Allow CloudWatch exporter SA created by Helm
             "system:serviceaccount:monitoring:prometheus-cloudwatch-exporter*"
           ]
         }
       }
     }]
   })
+
+  tags = {
+    Name = "${var.cluster_name}-prometheus-irsa-role"
+  }
 }
 
 ############################################
@@ -37,21 +47,27 @@ resource "aws_iam_policy" "prometheus_cloudwatch_policy" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "cloudwatch:GetMetricData",
-        "cloudwatch:GetMetricStatistics",
-        "cloudwatch:ListMetrics",
-        "tag:GetResources"
-      ]
-      Resource = "*"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:GetMetricData",
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:ListMetrics",
+          "tag:GetResources"
+        ]
+        Resource = "*"
+      }
+    ]
   })
+
+  tags = {
+    Name = "${var.cluster_name}-prometheus-cloudwatch-policy"
+  }
 }
 
 ############################################
-# Attach Policy to Role
+# Attach Policy to IRSA Role
 ############################################
 resource "aws_iam_role_policy_attachment" "prometheus_cloudwatch_attach" {
   role       = aws_iam_role.prometheus_irsa_role.name
