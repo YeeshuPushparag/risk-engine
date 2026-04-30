62c72195a04dd627505a4ca1242712b02720a67b
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-04-30 23:40:58 +0530

    testing

--- infra/eks/addons/main.tf
+++ infra/eks/addons/main.tf
@@ -1,3 +1,6 @@
+# -------------------------
+# aws-auth (READ EXISTING)
+# -------------------------
 data "kubernetes_config_map_v1" "aws_auth" {
   metadata {
     name      = "aws-auth"
@@ -5,6 +8,9 @@ data "kubernetes_config_map_v1" "aws_auth" {
   }
 }
 
+# -------------------------
+# aws-auth (PATCH)
+# -------------------------
 resource "kubernetes_config_map_v1" "aws_auth_patch" {
   metadata {
     name      = "aws-auth"
@@ -16,14 +22,22 @@ resource "kubernetes_config_map_v1" "aws_auth_patch" {
       {
         rolearn  = data.terraform_remote_state.cluster.outputs.node_role_arn
         username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = ["system:bootstrappers", "system:nodes"]
+        groups   = [
+          "system:bootstrappers",
+          "system:nodes"
+        ]
       }
     ])
   }
 
-  force = true
+  depends_on = [
+    data.kubernetes_config_map_v1.aws_auth
+  ]
 }
 
+# -------------------------
+# EBS CSI Addon
+# -------------------------
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name = data.terraform_remote_state.cluster.outputs.cluster_name
   addon_name   = "aws-ebs-csi-driver"
