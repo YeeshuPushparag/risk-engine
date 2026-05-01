45a1b7cf39a02abc9b6088c0e1b606b287e97d14
YeeshuPushparag <pushparagyeeshu@gmail.com>
2026-05-01 21:29:44 +0530

    fixed the s3 terraform bucket name

--- infra/eks/data.tf
+++ infra/eks/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "platform" {
   backend = "s3"
 
   config = {
-    bucket         = "risk-tf-state-platform-pushparag"
+    bucket         = "risk-tf-state-platform-yeeshu"
     key            = "platform/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "risk-tf-locks"
